5b232996de83827d4ecd76d355799ef9f383118e
komalll6 <komalkalra698@gmail.com>
2026-05-28 01:29:01 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+
+      <p>
+        Vite is a build tool that aims to provide a faster and leaner
+        development experience for modern web projects.
+      </p>
+    </div>
+  )
+}
+
+export default App
